4c4e456b0e7409c8d75a7ea027e032d0b2399095
longyue0521 <longyueli0521@gmail.com>
2021-10-02 18:37:40 +0800

    test(loganalyzer): add test case for good extension of log file

--- loganalyzer/loganalyzer_test.go
+++ loganalyzer/loganalyzer_test.go
@@ -12,3 +12,15 @@ func TestIsValidLogFileName_BadExtension_ReturnFalse(t *testing.T) {
 	result := analyzer.IsValidLogFileName("file.foo")
 	assert.Equal(t, false, result)
 }
+
+func TestIsValidLogFileName_GoodExtensionLowercase_ReturnTrue(t *testing.T) {
+	analyzer := LogAnalyzer{}
+	result := analyzer.IsValidLogFileName("file.slf")
+	assert.Equal(t, true, result)
+}
+
+func TestIsValidLogFileName_GoodExtensionUppercase_ReturnTrue(t *testing.T) {
+	analyzer := LogAnalyzer{}
+	result := analyzer.IsValidLogFileName("file.SLF")
+	assert.Equal(t, true, result)
+}
