63ad25a20633d28ea58c274147b15486e32dec31
longyue0521 <longyueli0521@gmail.com>
2021-10-02 18:13:31 +0800

    feat(loganalyzer): add LogAnalyzer struct and IsValidFileName Method which has a bug.

---
+++ loganalyzer/loganalyzer.go
@@ -0,0 +1,12 @@
+package loganalyzer
+
+import "strings"
+
+type LogAnalyzer struct{}
+
+func (l *LogAnalyzer) IsValidLogFileName(fileName string) bool {
+	if strings.HasSuffix(fileName, ".SLF") {
+		return false
+	}
+	return true
+}
