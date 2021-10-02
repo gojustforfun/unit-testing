3d4456c7705211ec855bf85764e9cd62d93d8632
longyue0521 <longyueli0521@gmail.com>
2021-10-02 17:53:51 +0800

    test(project): initialize base test environment for projects

---
+++ main_test.go
@@ -0,0 +1,11 @@
+package main_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNothing(t *testing.T) {
+	assert.Equal(t, true, true, "this is an test error message")
+}
